d0a3a91182623d477f87492e6c36375f328d45b8
Bryson Goto <gotob@oregonstate.edu>
2024-09-07 15:31:49 -1000

    Full test code with  modified library

--- 2_Programming/Hopper_Test/Hopper_Test.ino
+++ 2_Programming/Hopper_Test/Hopper_Test.ino
@@ -2,13 +2,16 @@
 // Source: https://www.instructables.com/Continuous-Rotation-Servo-Arduino-UNO-a-Tutorial/
 
 // Pin allocation
-#define SERVOPIN 8           // Servo control line on digital pin 8
-#define IR_SENSORPIN 9       // IR sensor output pin
-#define LED 10               // LED output if needed
+#define SERVOPIN 8           // Servo control line on digital pin 8, 5V
+#define IR_SENSORPIN 9       // IR sensor output pin, 3.3V, ADD 220 RESISTOR
+#define LED 10               // LED output if needed, ADD 220 RESISTOR
 
 Adafruit_SoftServo myServo;  // Create servo object
    
 void setup() {
+  
+  // Initiating Serial communication
+  Serial.begin(9600);
 
   // Initiating pins
   pinMode(IR_SENSORPIN, INPUT_PULLUP);
@@ -26,48 +29,38 @@ void loop()  {
 
   //sensorTest();
   //servoTest();
-  //fullTest();
+  fullTest();
 
   delay(500);                    
 }
 
 // Sensor blink test, if sensor detects something the LED will blink
 void sensorTest() {
-
   if (digitalRead(IR_SENSORPIN) == LOW) {
     digitalWrite(LED, HIGH);
-    delay(1000);
+    delay(500);
     digitalWrite(LED, LOW);
   }
-
 }
 
-// Servo speed control
+// Servo speed control with default function
 void servoControl(int speed) {
-
   myServo.write(speed);
   myServo.refresh();
-
 }
 
 // Servo test, sets speed to 5 random values
 void servoTest() {
-
   for (int i = 0; i < 5; i++) {
     servoControl(random(0,180));
     delay(1000);
   }
-
 }
 
 // Full test with IR sensor and servo
 void fullTest() {
-
-  if (digitalRead(IR_SENSORPIN) == LOW) {
+  if (digitalRead(IR_SENSORPIN) == LOW)
     servoControl(180);          // Servo is full speed
-  }
-  else {
-    servoControl(90);           // Servo is stationary
-  }
-
+  else
+    servoControl(0);            // Servo is stationary
 }
